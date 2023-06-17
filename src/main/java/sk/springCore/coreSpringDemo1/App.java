89a159a27fd8770ace054e0397d57060f97c2566
sachin karande <karandesd98@gmail.com>
2023-06-17 11:06:40 -0700

    normal

---
+++ src/main/java/sk/springCore/coreSpringDemo1/App.java
@@ -0,0 +1,17 @@
+package sk.springCore.coreSpringDemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World  in git hub!" );
+        
+        
+        
+        
+    }
+}
